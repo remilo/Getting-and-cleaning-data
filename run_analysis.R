a93c69889390afe978ac1309ef8a78ba321f5f81
remilo <remi_lau@yahoo.fr>
2017-06-18 17:47:47 +0200

    Add row.name=FALSE in writing the table tidy_data

--- run_analysis.R
+++ run_analysis.R
@@ -75,4 +75,4 @@ melt_data = melt(data, id = id_labels, measure.vars = data_labels)
 # Apply mean function to melt data and switch back to long format
 tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
 
-write.table(tidy_data, file = "./tidy_data.txt")
+write.table(tidy_data, file = "./tidy_data.txt", row.name=FALSE)
